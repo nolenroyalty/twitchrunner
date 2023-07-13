faa92e479bdbd8713d4acd5aa1df2ab75c7e3df0
Nolen Royalty <nolen@deepfield.net>
2023-07-13 17:05:01 -0400

    add basic UDP server

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,17 @@
 extends KinematicBody2D
 
-export(int)var SPEED = 100
-export(int)var ACCELERATION = 40
+export(int)var SPEED = 65
+export(int)var ACCELERATION = 35
 export(int)var GRAVITY_MAX = SPEED
-export(int)var GRAVITY_ACCEL = 20
+export(int)var GRAVITY_ACCEL = 17
 
 var velocity = Vector2()
+var autorun = true
 
-# ADD GRAVITY LOL
 func _physics_process(_delta):
 	var dx = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
+	if dx == 0 and autorun:
+		dx = 1
 	velocity.x = move_toward(velocity.x, dx * SPEED, ACCELERATION)
 	if not is_on_floor():
 		velocity.y = move_toward(velocity.y, GRAVITY_MAX, GRAVITY_ACCEL)
@@ -18,3 +20,6 @@ func _physics_process(_delta):
 		# if Input.is_action_just_pressed("player_jump"):
 		# 	velocity.y = -SPEED * 2
 	velocity = move_and_slide(velocity)
+
+func _ready():
+	$AnimatedSprite.play("run")
